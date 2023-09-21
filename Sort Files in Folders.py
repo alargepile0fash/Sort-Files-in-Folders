b27710dbea891df70e07ef5fe810119841254b7a
alargepile0fash <138829152+alargepile0fash@users.noreply.github.com>
2023-09-21 01:45:31 -0400

    Add files via upload
    added lines to prevent the code from delivering an error message if it doesn't find a particular file

--- Sort Files in Folders.py
+++ Sort Files in Folders.py
@@ -1,291 +1,66 @@
 import os
 import shutil
 
-path = "<Your Folder name here Format: C:/Users/User_name/Downloads/>"
-names =os.listdir(path)
+path = 'C:\\Users\\ashley\\Downloads\\'
 
-folder_name = ['Misc Files','Developer Files','Disk Image Files','Compressed Files','Settings Files','System Files','Plugin Files','Web Files','Game Files','Database','Spreadsheet','3d','Images','Text Files','Executables','Videos','Music','pdf','Presentation','Data Files']
-for x in range(0,19):
-    if not os.path.exists(path+folder_name[x]):
-        os.makedirs(path+folder_name[x])
+names = os.listdir(path)
 
-for files in names:
-    if ".png" in files :
-        shutil.move(path+files, path+'Images/'+files)
-    if ".jpg" in files :
-        shutil.move(path+files, path+'Images/'+files)
-    if ".tif" in files :
-        shutil.move(path+files, path+'Images/'+files)
-    if ".tiff" in files :
-        shutil.move(path+files, path+'Images/'+files)
-    if ".bmp" in files :
-        shutil.move(path+files, path+'Images/'+files)
-    if ".jpeg" in files :
-        shutil.move(path+files, path+'Images/'+files)
-    if ".gif" in files :
-        shutil.move(path+files, path+'Images/'+files)
-    if ".eps" in files :
-        shutil.move(path+files, path+'Images/'+files)
-    if ".raw" in files :
-        shutil.move(path+files, path+'Images/'+files)
-    if ".cr2" in files :
-        shutil.move(path+files, path+'Images/'+files)
-    if ".nef" in files :
-        shutil.move(path+files, path+'Images/'+files)
-    if ".orf" in files :
-        shutil.move(path+files, path+'Images/'+files)
-    if ".sr2" in files :
-        shutil.move(path+files, path+'Images/'+files)
-    if ".txt" in files:
-        shutil.move(path+files, path+'Text Files/'+files)
-    if ".fpt" in files :
-        shutil.move(path+files, path+'Text Files/'+files)
-    if ".docx" in files :
-        shutil.move(path+files, path+'Text Files/'+files)
-    if ".rtf" in files :
-        shutil.move(path+files, path+'Text Files/'+files)
-    if ".log" in files :
-        shutil.move(path+files, path+'Text Files/'+files)
-    if ".doc" in files :
-        shutil.move(path+files, path+'Text Files/'+files)
-    if ".ppt" in files :
-        shutil.move(path+files, path+'Presentation/'+files)
-    if ".pptx" in files :
-        shutil.move(path+files, path+'Presentation/'+files)
-    if ".csv" in files :
-        shutil.move(path+files, path+'Data Files/'+files)
-    if ".key" in files :
-        shutil.move(path+files, path+'Data Files/'+files)
-    if ".keychain" in files :
-        shutil.move(path+files, path+'Data Files/'+files)
-    if ".dat" in files :
-        shutil.move(path+files, path+'Data Files/'+files)
-    if ".sdf" in files :
-        shutil.move(path+files, path+'Data Files/'+files)
-    if ".tar" in files :
-        shutil.move(path+files, path+'Data Files/'+files)
-    if ".xml" in files :
-        shutil.move(path+files, path+'Data Files/'+files)
-    if ".vcf" in files :
-        shutil.move(path+files, path+'Data Files/'+files)
-    if ".aif" in files :
-        shutil.move(path+files, path+'Music/'+files)
-    if ".iff" in files :
-        shutil.move(path+files, path+'Music/'+files)
-    if ".m3u" in files :
-        shutil.move(path+files, path+'Music/'+files)
-    if ".m4a" in files :
-        shutil.move(path+files, path+'Music/'+files)
-    if ".mid" in files :
-        shutil.move(path+files, path+'Music/'+files)
-    if ".mp3" in files :
-        shutil.move(path+files, path+'Music/'+files)
-    if ".mpa" in files :
-        shutil.move(path+files, path+'Music/'+files)
-    if ".wav" in files :
-        shutil.move(path+files, path+'Music/'+files)
-    if ".wma" in files :
-        shutil.move(path+files, path+'Music/'+files)
-    if ".3g2" in files :
-        shutil.move(path+files, path+'Videos/'+files)
-    if ".3gp" in files :
-        shutil.move(path+files, path+'Videos/'+files)
-    if ".asf" in files :
-        shutil.move(path+files, path+'Videos/'+files)
-    if ".avi" in files :
-        shutil.move(path+files, path+'Videos/'+files)
-    if ".flv" in files :
-        shutil.move(path+files, path+'Videos/'+files)
-    if ".m4v" in files :
-        shutil.move(path+files, path+'Videos/'+files)
-    if ".mov" in files :
-        shutil.move(path+files, path+'Videos/'+files)
-    if ".mp4" in files :
-        shutil.move(path+files, path+'Videos/'+files)
-    if ".mpg" in files :
-        shutil.move(path+files, path+'Videos/'+files)
-    if ".rm" in files :
-        shutil.move(path+files, path+'Videos/'+files)
-    if ".srt" in files :
-        shutil.move(path+files, path+'Videos/'+files)
-    if ".swf" in files :
-        shutil.move(path+files, path+'Videos/'+files)
-    if ".mkv" in files :
-        shutil.move(path+files, path+'Videos/'+files)
-    if ".vob" in files :
-        shutil.move(path+files, path+'Videos/'+files)
-    if ".wmv" in files :
-        shutil.move(path+files, path+'Videos/'+files)
-    if ".3dm" in files :
-        shutil.move(path+files, path+'3d/'+files)
-    if ".3ds" in files :
-        shutil.move(path+files, path+'3d/'+files)
-    if ".max" in files :
-        shutil.move(path+files, path+'3d/'+files)
-    if ".obj" in files :
-        shutil.move(path+files, path+'3d/'+files)
-    if ".xlr" in files :
-        shutil.move(path+files, path+'Spreadsheet/'+files)
-    if ".xls" in files :
-        shutil.move(path+files, path+'Spreadsheet/'+files)
-    if ".xlsx" in files :
-        shutil.move(path+files, path+'Spreadsheet/'+files)
-    if ".db" in files :
-        shutil.move(path+files, path+'Database/'+files)
-    if ".sql" in files :
-        shutil.move(path+files, path+'Database/'+files)
-    if ".accdb" in files :
-        shutil.move(path+files, path+'Database/'+files)
-    if ".dbf" in files :
-        shutil.move(path+files, path+'Database/'+files)
-    if ".mdb" in files :
-        shutil.move(path+files, path+'Database/'+files)
-    if ".pdb" in files :
-        shutil.move(path+files, path+'Database/'+files)
-    if ".apk" in files :
-        shutil.move(path+files, path+'Executables/'+files)
-    if ".app" in files :
-        shutil.move(path+files, path+'Executables/'+files)
-    if ".bat" in files :
-        shutil.move(path+files, path+'Executables/'+files)
-    if ".cgi" in files :
-        shutil.move(path+files, path+'Executables/'+files)
-    if ".com" in files :
-        shutil.move(path+files, path+'Executables/'+files)
-    if ".exe" in files :
-        shutil.move(path+files, path+'Executables/'+files)
-    if ".gadget" in files :
-        shutil.move(path+files, path+'Executables/'+files)
-    if ".jar" in files :
-        shutil.move(path+files, path+'Executables/'+files)
-    if ".wsf" in files :
-        shutil.move(path+files, path+'Executables/'+files)
-    if ".b" in files :
-        shutil.move(path+files, path+'Game Files/'+files)
-    if ".dem" in files :
-        shutil.move(path+files, path+'Game Files/'+files)
-    if ".gam" in files :
-        shutil.move(path+files, path+'Game Files/'+files)
-    if ".nes" in files :
-        shutil.move(path+files, path+'Game Files/'+files)
-    if ".rom" in files :
-        shutil.move(path+files, path+'Game Files/'+files)
-    if ".sav" in files :
-        shutil.move(path+files, path+'Game Files/'+files)
-    if ".pdf" in files :
-        shutil.move(path+files, path+'pdf/'+files)
-    if ".asp" in files :
-        shutil.move(path+files, path+'Web Files/'+files)
-    if ".aspx" in files :
-        shutil.move(path+files, path+'Web Files/'+files)
-    if ".css" in files :
-        shutil.move(path+files, path+'Web Files/'+files)
-    if ".htm" in files :
-        shutil.move(path+files, path+'Web Files/'+files)
-    if ".html" in files :
-        shutil.move(path+files, path+'Web Files/'+files)
-    if ".js" in files :
-        shutil.move(path+files, path+'Web Files/'+files)
-    if ".jsp" in files :
-        shutil.move(path+files, path+'Web Files/'+files)
-    if ".php" in files :
-        shutil.move(path+files, path+'Web Files/'+files)
-    if ".rss" in files :
-        shutil.move(path+files, path+'Web Files/'+files)
-    if ".crx" in files :
-        shutil.move(path+files, path+'Plugin Files/'+files)
-    if ".plugin" in files :
-        shutil.move(path+files, path+'Plugin Files/'+files)
-    if ".fnt" in files :
-        shutil.move(path+files, path+'Plugin Files/'+files)
-    if ".fon" in files :
-        shutil.move(path+files, path+'Plugin Files/'+files)
-    if ".otf" in files :
-        shutil.move(path+files, path+'Plugin Files/'+files)
-    if ".ttf" in files :
-        shutil.move(path+files, path+'Plugin Files/'+files)
-    if ".cab" in files :
-        shutil.move(path+files, path+'System Files/'+files)
-    if ".deskthemepack" in files :
-        shutil.move(path+files, path+'System Files/'+files)
-    if ".dll" in files :
-        shutil.move(path+files, path+'System Files/'+files)
-    if ".ico" in files :
-        shutil.move(path+files, path+'System Files/'+files)
-    if ".sys" in files :
-        shutil.move(path+files, path+'System Files/'+files)
-    if ".lnk" in files :
-        shutil.move(path+files, path+'System Files/'+files)
-    if ".dmp" in files :
-        shutil.move(path+files, path+'System Files/'+files)
-    if ".drv" in files :
-        shutil.move(path+files, path+'System Files/'+files)
-    if ".ini" in files :
-        shutil.move(path+files, path+'Settings Files/'+files)
-    if ".cfg" in files :
-        shutil.move(path+files, path+'Settings Files/'+files)
-    if ".prf" in files :
-        shutil.move(path+files, path+'Settings Files/'+files)
-    if ".7z" in files :
-        shutil.move(path+files, path+'Compressed Files/'+files)
-    if ".cbr" in files :
-        shutil.move(path+files, path+'Compressed Files/'+files)
-    if ".deb" in files :
-        shutil.move(path+files, path+'Compressed Files/'+files)
-    if ".gz" in files :
-        shutil.move(path+files, path+'Compressed Files/'+files)
-    if ".pkg" in files :
-        shutil.move(path+files, path+'Compressed Files/'+files)
-    if ".rar" in files :
-        shutil.move(path+files, path+'Compressed Files/'+files)
-    if ".rpm" in files :
-        shutil.move(path+files, path+'Compressed Files/'+files)
-    if ".tar.gz" in files :
-        shutil.move(path+files, path+'Compressed Files/'+files)
-    if ".zip" in files :
-        shutil.move(path+files, path+'Compressed Files/'+files)
-    if ".zipx" in files :
-        shutil.move(path+files, path+'Compressed Files/'+files)
-    if ".bin" in files :
-        shutil.move(path+files, path+'Disk Image Files/'+files)
-    if ".dmg" in files :
-        shutil.move(path+files, path+'Disk Image Files/'+files)
-    if ".iso" in files :
-        shutil.move(path+files, path+'Disk Image Files/'+files)
-    if ".mdf" in files :
-        shutil.move(path+files, path+'Disk Image Files/'+files)
-    if ".vcd" in files :
-        shutil.move(path+files, path+'Disk Image Files/'+files)
-    if ".c" in files :
-        shutil.move(path+files, path+'Developer Files/'+files)
-    if ".class" in files :
-        shutil.move(path+files, path+'Developer Files/'+files)
-    if ".cpp" in files :
-        shutil.move(path+files, path+'Developer Files/'+files)
-    if ".cs" in files :
-        shutil.move(path+files, path+'Developer Files/'+files)
-    if ".java" in files :
-        shutil.move(path+files, path+'Developer Files/'+files)
-    if ".pl" in files :
-        shutil.move(path+files, path+'Developer Files/'+files)
-    if ".py" in files :
-        shutil.move(path+files, path+'Developer Files/'+files)
-    if ".sh" in files :
-        shutil.move(path+files, path+'Developer Files/'+files)
-    if ".vb" in files :
-        shutil.move(path+files, path+'Developer Files/'+files)
-    if ".vcxproj" in files :
-        shutil.move(path+files, path+'Developer Files/'+files)
-    if ".jsp" in files :
-        shutil.move(path+files, path+'Developer Files/'+files)
-    if ".servlet" in files :
-        shutil.move(path+files, path+'Developer Files/'+files)
-    if ".ics" in files :
-        shutil.move(path+files, path+'Misc Files/'+files)
-    if ".msi" in files :
-        shutil.move(path+files, path+'Misc Files/'+files)
-    if ".part" in files :
-        shutil.move(path+files, path+'Misc Files/'+files)
-    if ".torrent" in files :
-        shutil.move(path+files, path+'Misc Files/'+files)
-    
+folder_name = ['Misc Files', 'Developer Files', 'Disk Image Files', 'Compressed Files', 'Settings Files', 'System Files', 'Plugin Files', 'Web Files', 'Game Files', 'Database', 'Spreadsheet', '3d', 'Images', 'Text Files', 'Executables', 'Videos', 'Music', 'pdf', 'Presentation', 'Data Files']
+
+for x in range(0, 19):
+    if not os.path.exists(os.path.join(path, folder_name[x])):
+        os.makedirs(os.path.join(path, folder_name[x]))
+
+for file_name in names:
+    if os.path.isfile(os.path.join(path, file_name)):
+        extension = os.path.splitext(file_name)[1].lower()  # Get the file extension in lowercase
+
+        destination_folder = None
+
+        if ".png" in extension or ".jpg" in extension or ".tif" in extension or ".tiff" in extension or ".bmp" in extension or ".jpeg" in extension or ".gif" in extension or ".eps" in extension or ".raw" in extension or ".cr2" in extension or ".nef" in extension or ".orf" in extension or ".sr2" in extension or ".heic" in extension:
+            destination_folder = 'Images'
+        elif ".txt" in extension or ".fpt" in extension or ".docx" in extension or ".rtf" in extension or ".log" in extension or ".doc" in extension:
+            destination_folder = 'Text Files'
+        # Add more conditions for other file extensions as needed
+        elif ".ppt" in extension or ".pptx" in extension:
+            destination_folder = 'Presentation'
+        elif ".csv" in extension or ".key" in extension or ".keychain" in extension or ".dat" in extension or ".sdf" in extension or ".tar" in extension or ".xml" in extension or ".vcf" in extension:
+            destination_folder = 'Data Files'
+        elif ".aif" in extension or ".iff" in extension or ".m3u" in extension or ".m4a" in extension or ".mid" in extension or ".mp3" in extension or ".mpa" in extension or ".wav" in extension or ".wma" in extension:
+            destination_folder = 'Music'
+        elif ".3g2" in extension or ".3gp" in extension or ".asf" in extension or ".avi" in extension or ".flv" in extension or ".m4v" in extension or ".mov" in extension or ".mp4" in extension or ".mpg" in extension or ".rm" in extension or ".srt" in extension or ".swf" in extension or ".mkv" in extension or ".vob" in extension or ".wmv" in extension:
+            destination_folder = 'Videos'
+        elif ".3dm" in extension or ".3ds" in extension or ".max" in extension or ".obj" in extension:
+            destination_folder = '3d'
+        elif ".xls" in extension or ".xlsx" in extension or ".db" in extension or ".sql" in extension or ".accdb" in extension or ".dbf" in extension or ".mdb" in extension or ".pdb" in extension:
+            destination_folder = 'Database'
+        elif ".apk" in extension or ".app" in extension or ".bat" in extension or ".cgi" in extension or ".com" in extension or ".exe" in extension or ".gadget" in extension or ".jar" in extension or ".wsf" in extension:
+            destination_folder = 'Executables'
+        elif ".b" in extension or ".dem" in extension or ".gam" in extension or ".nes" in extension or ".rom" in extension or ".sav" in extension:
+            destination_folder = 'Game Files'
+        elif ".pdf" in extension:
+            destination_folder = 'pdf'
+        elif ".asp" in extension or ".aspx" in extension or ".css" in extension or ".htm" in extension or ".html" in extension or ".js" in extension or ".jsp" in extension or ".php" in extension or ".rss" in extension or ".crx" in extension or ".plugin" in extension or ".fnt" in extension or ".fon" in extension or ".otf" in extension or ".ttf" in extension:
+            destination_folder = 'Web Files'
+        elif ".cab" in extension or ".deskthemepack" in extension or ".dll" in extension or ".ico" in extension or ".sys" in extension or ".lnk" in extension or ".dmp" in extension or ".drv" in extension:
+            destination_folder = 'System Files'
+        elif ".ini" in extension or ".cfg" in extension or ".prf" in extension:
+            destination_folder = 'Settings Files'
+        elif ".7z" in extension or ".cbr" in extension or ".deb" in extension or ".gz" in extension or ".pkg" in extension or ".rar" in extension or ".rpm" in extension or ".tar.gz" in extension or ".zip" in extension or ".zipx" in extension:
+            destination_folder = 'Compressed Files'
+        elif ".bin" in extension or ".dmg" in extension or ".iso" in extension or ".mdf" in extension or ".vcd" in extension:
+            destination_folder = 'Disk Image Files'
+
+        if destination_folder is not None:
+            source_file_path = os.path.join(path, file_name)
+            destination_file_path = os.path.join(path, destination_folder, file_name)
+
+            try:
+                shutil.move(source_file_path, destination_file_path)
+                print(f"Moved file '{file_name}' to '{destination_folder}' successfully.")
+            except FileNotFoundError:
+                print(f"File '{file_name}' not found in the source directory. Skipping.")
+            except Exception as e:
+                print(f"An error occurred while processing '{file_name}': {str(e)}")
+
+print("Processing completed.")
